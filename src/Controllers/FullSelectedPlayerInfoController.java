ce03f3d7dddf6aed968b4528e339984c2d664985
Rachit Patel <56652910+RachitPatel141026@users.noreply.github.com>
2020-12-11 14:50:28 -0500

    Added button in playerSearchView.fxml to change scene and get full player details and in which team he belongs with whole information

--- src/Controllers/FullSelectedPlayerInfoController.java
+++ src/Controllers/FullSelectedPlayerInfoController.java
@@ -3,9 +3,15 @@ package Controllers;
 import Models.PlayerInfo;
 import com.google.gson.JsonObject;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -41,32 +47,43 @@ public class FullSelectedPlayerInfoController implements Initializable {
     @FXML
     private Label abbreviationLabel;
 
-     private PlayerInfo playerInfo;
-
-     private JsonObject teamInfo;
+    private PlayerInfo playerInfo;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
 
     }
 
 
     public void unitData(PlayerInfo selectedPlayerInfo) {
+        playerInfo = selectedPlayerInfo;
+        JsonObject teamInfo = selectedPlayerInfo.getTeamInfo();
 
-         playerInfo = selectedPlayerInfo;
-         teamInfo = selectedPlayerInfo.getTeamInfo();
+        firstNameLabel.setText("First Name:"+ selectedPlayerInfo.getFirstName());
+        lastNameLabel.setText("Last Name:"+ selectedPlayerInfo.getLastName());
+        positionLabel.setText("Position:"+ selectedPlayerInfo.getPosition());
+        heightLabel.setText("height:"+ selectedPlayerInfo.getHeightInches()+" inch");
+        weightLabel.setText("Weight:"+ selectedPlayerInfo.getWeight()+" lb");
+        teamNameLabel.setText("Team Name:"+ teamInfo.get("full_name"));
+        cityLabel.setText("City:"+ teamInfo.get("city"));
+        conferenceLabel.setText("Conference:"+ teamInfo.get("conference"));
+        divisionLabel.setText("Division:"+ teamInfo.get("division"));
+        abbreviationLabel.setText("Abbreviation:"+ teamInfo.get("abbreviation"));
 
-        firstNameLabel.setText("First Name:"+playerInfo.getFirstName());
-        lastNameLabel.setText("Last Name:"+playerInfo.getLastName());
-        positionLabel.setText("Position:"+playerInfo.getPosition());
-        heightLabel.setText("height:"+playerInfo.getHeightInches()+" inch");
-        weightLabel.setText("Weight:"+playerInfo.getWeight()+" lb");
-        teamNameLabel.setText("Team Name:"+teamInfo.get("full_name"));
-        cityLabel.setText("City:"+teamInfo.get("city"));
-        conferenceLabel.setText("Conference:"+teamInfo.get("conference"));
-        divisionLabel.setText("Division:"+teamInfo.get("division"));
-        abbreviationLabel.setText("Abbreviation:"+teamInfo.get("abbreviation"));
+    }
 
+    public void changeSceneToPlayerSearch(javafx.event.ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../Views/playerSearchView.fxml"));
+        Parent parent = loader.load();
+        Scene scene = new Scene(parent);
+        PlayerSearchController c = loader.getController();
+        c.fillTheScene(playerInfo.getFirstName());
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
 
--- src/Controllers/PlayerSearchController.java
+++ src/Controllers/PlayerSearchController.java
@@ -4,18 +4,19 @@ import Models.ApiResponseModel;
 import Models.PlayerInfo;
 import Utilities.JsonFileUtility;
 import Utilities.NBAApiUtility;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -41,7 +42,10 @@ public class PlayerSearchController implements Initializable {
     @FXML
     private Label numberShowingLabel;
 
+    @FXML
+    private Button fullPlayerStatsButton;
 
+    private PlayerInfo playerInfos;
     private ApiResponseModel response;
 
     @Override
@@ -52,14 +56,16 @@ public class PlayerSearchController implements Initializable {
         listView.getSelectionModel().selectedItemProperty().addListener((observableValue, playerInfo, t1) -> {
             firstNameLabel.setText("First Name :"+t1.getFirstName());
             lastNameLabel.setText("Last Name :"+t1.getLastName());
-
+            playerInfos = listView.getSelectionModel().getSelectedItem();
         });
+
     }
 
     @FXML
     private void searchButton()
     {
         String searchText = searchTextField.getText();
+
         searchText = searchText.replace(" ", "%20");
 
         //call the API and save the JSON file to the hard drive
@@ -79,17 +85,34 @@ public class PlayerSearchController implements Initializable {
         listView.getItems().addAll(response.getPlayers());
         numberShowingLabel.setText("Total Number of rows in Table: "+listView.getItems().size());
     }
+    @FXML
+    public void Scene(ActionEvent event) throws IOException {
 
-    public void getFullStats(javafx.event.ActionEvent event) throws IOException
-    {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(new Object(){}.getClass().getResource("Views/fullSelectedPlayerInfo.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setScene(scene);
-        window.setTitle("Player Detail");
-        window.show();
+        loader.setLocation(getClass().getResource("../Views/fullPlayerView.fxml"));
+        Parent parent = loader.load();
+        Scene scene = new Scene(parent);
+        FullSelectedPlayerInfoController c = loader.getController();
+        c.unitData(playerInfos);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
+
+    @FXML
+    public void fillTheScene(String searchText)
+    {
+        searchText = searchText.replace(" ","%20");
+
+       //call the API and save the JSON file to the hard drive
+        NBAApiUtility.getPlayerInfoFromSearch(searchText);
+
+        //read the json file and create an APIResponseModel
+        File jsonFile = new File("src/Utilities/players.json");
+        response = JsonFileUtility.getApiInfoFromJson(jsonFile);
+
+        updateScene();
+    }
+
 }
 
